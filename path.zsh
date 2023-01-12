0e1265f568d21717b46cd5ba9dfa05d0fc7e248d
Arseny Egorov <egoroff-ars@yandex.ru>
2023-01-12 21:50:47 +0100

    Move PATHs to separate file

---
+++ path.zsh
@@ -0,0 +1,19 @@
+export PATH="$HOME/.poetry/bin:$PATH"
+export PATH="/usr/local/opt/ruby/bin:$PATH"
+export PATH="/usr/local/opt/libpq/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/dotfiles/bin:$PATH"
+export PATH="/Users/u17485588/Library/Application Support/Coursier/bin:$PATH"
+export PATH="/usr/local/sbin:$PATH" # rabbitmq
+export PATH="$PATH:$HOME/.pub-cache/bin" # flutterfile
+export PATH="/usr/local/lib/ruby/gems/3.1.0/bin:$PATH"
+# go
+export GOPATH="$HOME/go"
+export PATH="$PATH:$GOPATH/bin"
+# fh
+export PATH="$HOME/Library/Python/3.9/bin:$PATH"
+export PATH="$HOME/code/search-data-pipelines/bin:$PATH"
+export PATH="$HOME/nvim/bin:$PATH"
+# rust
+export PATH="/Users/mark/.cargo/bin:$PATH"
+
