39d1b0eebda51b7631d115676338f2646c4064c6
Arseny Egorov <egoroff-ars@yandex.ru>
2024-04-25 16:18:04 +0200

    rebase

--- nvim/lua/marchinho/packer.lua
+++ nvim/lua/marchinho/packer.lua
@@ -9,16 +9,17 @@ return require('packer').startup(function(use)
     use { 'junegunn/fzf', run = ":call fzf#install()" }
     use 'junegunn/fzf.vim'
     use 'tpope/vim-fugitive'
-    use 'ellisonleao/gruvbox.nvim'
-    use 'nyoom-engineering/oxocarbon.nvim'
-    use 'projekt0n/github-nvim-theme'
     use 'nvim-treesitter/nvim-treesitter'
     use 'APZelos/blamer.nvim'
     use 'lewis6991/gitsigns.nvim'
     use 'terrastruct/d2-vim'
-    use 'fatih/vim-go'
     use 'ziglang/zig.vim'
     use 'vim-crystal/vim-crystal'
+    use 'fatih/vim-go'
+    -- Themes
+    use 'ellisonleao/gruvbox.nvim'
+    use 'projekt0n/github-nvim-theme'
+    use 'nyoom-engineering/oxocarbon.nvim'
     use 'rebelot/kanagawa.nvim'
     use {
         'VonHeikemen/lsp-zero.nvim',
